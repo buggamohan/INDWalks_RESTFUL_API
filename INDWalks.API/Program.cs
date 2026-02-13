3c40d952120d42166289174dec6b4b75b55a6187
buggamohan <143516130+buggamohan@users.noreply.github.com>
2026-02-13 23:50:42 +0530

    Update Program.cs

--- INDWalks.API/Program.cs
+++ INDWalks.API/Program.cs
@@ -35,11 +35,11 @@ namespace INDWalks.API
             var app = builder.Build();
 
             // Configure the HTTP request pipeline.
-            if (app.Environment.IsDevelopment())
-            {
+            
+            
                 app.UseSwagger();
                 app.UseSwaggerUI();
-            }
+            
 
             app.UseHttpsRedirection();
 
